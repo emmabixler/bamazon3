eaae642b924178bd73b731fff10ac273077a9f6d
leb <leb5313@gmail.com>
2019-04-07 23:42:08 -0400

    updated README

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # bamazon3
+
+
+Node.js & MySQL actvity
+
+
+Amazon- like storefront that a customer can request an item and the quanity.
+
+Create database in mySQL
+Create js file for bamazonCustomer 
+npm install 
+
--- bamazonCustomer1.js
+++ bamazonCustomer1.js
@@ -1,5 +1,5 @@
-var mysql = require("mysql");
 var inquirer = require("inquirer");
+var mysql = require("mysql");
 
 //setting up the port
 var connection = mysql.createConnection({
@@ -9,6 +9,28 @@ var connection = mysql.createConnection({
   password: "",
   database: "bamazon."
 });
-connection.connect(function(error) {
-  if (error) throw error;
+connection.connect(function(err) {
+  if (err) throw err;
+  bamazon();
+  console.log("connected as id" + connection.threadId);
 });
+//display invntory
+function bamazon() {
+  console.log("welcome");
+  connection.query("SELECT* FROM products", function(err, res) {
+    if (err) throw err;
+  });
+}
+//customers input
+inquirer.prompt([
+  {
+    type: "number",
+    message: "Enter name of product you would like to buy",
+    name: "id"
+  },
+  {
+    type: "number",
+    message: "How many would you like to purchase",
+    name: "quantity"
+  }
+]);
